94b8697f1c23590bff05059912b5aa0102d1f499
Lior Nabat <lior.nabat@gmail.clom>
2022-07-18 15:03:48 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
     css: ['primevue/resources/themes/saga-blue/theme.css',
         'primevue/resources/primevue.css',
         'primeicons/primeicons.css'],
+    build: {
+        transpile: ['primevue']
+    },
     modules: [
         '@vueuse/nuxt',
         '@pinia/nuxt'],
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -3,10 +3,12 @@ import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import Textarea from 'primevue/textarea';
 
+
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, {ripple: true});
+    nuxtApp.vueApp.use(PrimeVue, {ripple: false,});
     nuxtApp.vueApp.component('Button', Button);
     nuxtApp.vueApp.component('Textarea', Textarea);
+
 
     //other components that you need
 });
